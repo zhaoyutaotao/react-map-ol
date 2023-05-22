4f48f93cba92f26ab73320a97e43055dacc23777
zhaoyutao <1048@163.com>
2023-05-22 15:33:06 +0800

    添加base

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
+  base: '/react-map-ol/',
+  publicPath:'/react-map-ol/',
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'react-map-ol',
