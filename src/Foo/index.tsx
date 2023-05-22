7cef3f09298c70f934e4fac398ee8c21449ba093
zhaoyutao <1048@163.com>
2023-05-22 15:44:19 +0800

    测试page

--- src/Foo/index.tsx
+++ src/Foo/index.tsx
@@ -1,5 +1,11 @@
+/*
+ * @Author: zhaoyutao 1048@163.com
+ * @Date: 2023-05-22 12:24:29
+ * @LastEditors: zhaoyutao 1048@163.com
+ * @Description: 
+ */
 import React, { type FC } from 'react';
 
-const Foo: FC<{ title: string }> = (props) => <h4>{props.title}</h4>;
+const Foo: FC<{ title: string }> = (props) => <h4>{props.title}测试</h4>;
 
 export default Foo;
